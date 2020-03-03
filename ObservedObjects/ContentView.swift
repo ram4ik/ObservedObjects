54646fd31452d38d34c150b1d23af3d53c577b8d
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-03-03 14:22:29 +0200

    init

--- ObservedObjects/ContentView.swift
+++ ObservedObjects/ContentView.swift
@@ -9,8 +9,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    
+    @State private var street = ""
+    @State private var city = ""
+    @State private var country = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            Form {
+                Section(header: Text("Name")) {
+                    TextField("First name", text: $firstName)
+                    TextField("Last name", text: $lastName)
+                }
+                Section(header: Text("Location")) {
+                    TextField("Street", text: $street)
+                    TextField("City", text: $city)
+                    TextField("Country", text: $country)
+                }
+            }
+            Divider()
+            Group {
+                Text("First name is \(firstName)")
+                Text("Last name is \(lastName)")
+                Text("Street is \(street)")
+                Text("City is \(city)")
+                Text("Country is \(country)")
+            }.padding()
+        }
     }
 }
 
